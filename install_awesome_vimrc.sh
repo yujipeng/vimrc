15974bd4b28c44c587e6ed2618bc34d10a966d36
Jeep Yu <jeepyu@shijiebang.net>
2016-06-02 18:01:46 +0800

    修正vim输出

--- install_awesome_vimrc.sh
+++ install_awesome_vimrc.sh
@@ -2,11 +2,9 @@
 
 REAL_PARH=$(cd `dirname $0`; pwd)"/"
 
-
 if [ ! -d "~/.vim" ]; then
     mkdir ~/.vim 
 fi
-
 
 # 基本配置
 cp $REAL_PARH"basic.vim" ~/.vim/
@@ -15,8 +13,7 @@ cp $REAL_PARH"basic.vim" ~/.vim/
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp $REAL_PARH"vundle.vim" ~/.vim/
 cp $REAL_PARH"plugin.vim" ~/.vim/
-vim +PluginInstall +qall
-
+vim +PluginInstall +qall >> /dev/null
 
 echo '
 
